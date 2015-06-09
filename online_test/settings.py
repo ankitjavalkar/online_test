6632f52baaf3ef98610dc56db817338038a1c69f
ankitjavalkar <ankitrj.iitb@gmail.com>
2015-06-09 16:42:21 +0530

    Multiple fixes to errors after code-rearrangement - Remove evaluators from evaluators dir - Language IDs in forms has been fixed, all lower case - Remove spoken-tutorial database from settings

--- online_test/settings.py
+++ online_test/settings.py
@@ -63,14 +63,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'spoken_tutorial' : {
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME'   : 'YOUR DATABASE',
-        'USER'   : 'YOUR USERNAME',
-        'PASSWORD': 'YOUR PASSWORD',
-        'HOST'    :'',
-        'PORT'    :'',
-    }
 }
 
 # Internationalization
